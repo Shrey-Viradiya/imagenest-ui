b926811358debf8ff668ff61f850f9e0cededbc3
Shrey Viradiya <shrey.viradiya.100@outlook.com>
2024-06-13 21:26:26 +0530

    Caching the pins

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,21 +17,40 @@ function shuffleArray(array) {
     return array;
   }
 
-const HomePage = () => {
-  const [pins, setPins] = useState([]);
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/pins/board/1')
-      .then(response => response.json())
-      .then(data => setPins(shuffleArray(data)))
-      .catch(error => console.error('Error:', error));
-  }, []);
-
-  return (
-    <PinContainer>
-      {pins.map(pin => <PinCard key={pin.id} pin={pin} />)}
-    </PinContainer>
-  );
-};
-
-export default HomePage;
+  const HomePage = () => {
+    const [pins, setPins] = useState([]);
+  
+    useEffect(() => {
+      // Try to get data from localStorage
+      const cachedPins = localStorage.getItem('pins');
+      const timestamp = localStorage.getItem('timestamp');
+  
+      const oneMinute = 60 * 1000; // 60,000 milliseconds
+      const isDataOld = Date.now() - timestamp > oneMinute;
+  
+      if (cachedPins && !isDataOld) {
+        // If there's data in localStorage and it's not old, use it
+        setPins(shuffleArray(JSON.parse(cachedPins)));
+      } else {
+        // If there's no data in localStorage or it's old, fetch it
+        fetch('http://127.0.0.1:8000/pins/')
+          .then(response => response.json())
+          .then(data => {
+            // Store the fetched data and the current timestamp in localStorage
+            localStorage.setItem('pins', JSON.stringify(data));
+            localStorage.setItem('timestamp', Date.now());
+            setPins(shuffleArray(data));
+          })
+          .catch(error => console.error('Error:', error));
+      }
+    }, []);
+  
+    return (
+      <PinContainer>
+        {pins.map(pin => <PinCard key={pin.id} pin={pin} />)}
+      </PinContainer>
+    );
+  };
+  
+  export default HomePage;
+  
